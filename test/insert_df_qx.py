ebf9463b3ce2adc2cd0b0ee1265e5e74c2be8497
jinxin163 <jinxinphy@163.com>
2021-06-29 11:08:04 +0800

    2020-06-29

--- test/insert_df_qx.py
+++ test/insert_df_qx.py
@@ -3,16 +3,18 @@ import pandas as pd
 import time
 from config import resultCli
 
-df = pd.read_csv(r"skmyl.csv", sep='\t', header=None)
+df = pd.read_csv(r"qz_risk_zone_rain.csv", sep=',')
+df.drop(columns=['id'], inplace=True)
+df = df.tail(4000)
 print(df)
 
-df1 = resultCli.query_params(table='sqxj_hj_biz_067_qx_skmyl_valid_old111', items=["*"])
-print(df1)
-cols = list(df1.columns)
-cols.remove("id")
-df.columns = cols
-
-df = df[df['city'] == "衢州"]
+# df1 = resultCli.query_params(table='sqxj_hj_biz_067_qx_skmyl_valid_old111', items=["*"])
+# print(df1)
+# cols = list(df1.columns)
+# cols.remove("id")
+# df.columns = cols
+#
+# df = df[df['city'] == "衢州"]
 
 
 code_list = df['hash_unique'].drop_duplicates().to_list()
@@ -23,4 +25,4 @@ for code in code_list:
     dfi = dfi.head(1)
     print(dfi)
     resultCli.insert_df(table='qz_risk_zone_rain', df=dfi)
-    time.sleep(5)
+    time.sleep(2)
