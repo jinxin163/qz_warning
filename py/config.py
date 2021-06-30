74fc1ed4e884967409fd568e6d6af58f08bc5777
jinxin163 <jinxinphy@163.com>
2021-06-30 15:28:36 +0800

    2020-06-30

--- py/config.py
+++ py/config.py
@@ -11,13 +11,13 @@ rootPath = __curPath[:__curPath.find(__projectName) + len(__projectName)]
 conf = configparser.ConfigParser()
 conf.read(rootPath + r'/conf/conf.ini', encoding='utf-8')
 
-# _section1 = 'mysql_conn_zwy'
-# _section2 = 'mysql_conn_zwy'
-# _section3 = 'redis_conn_zwy'
+_section1 = 'mysql_conn_zwy'
+_section2 = 'mysql_conn_zwy'
+_section3 = 'redis_conn_zwy'
 
-_section1 = 'source_conn'
-_section2 = 'result_conn'
-_section3 = 'redis_conn'
+# _section1 = 'source_conn'
+# _section2 = 'result_conn'
+# _section3 = 'redis_conn'
 
 sourceCli = mysqlClient(host=conf.get(_section1, 'ip'), port=conf.getint(_section1, 'port'),
                         user=conf.get(_section1, 'user'), password=conf.get(_section1, 'pw'),
@@ -26,10 +26,10 @@ resultCli = mysqlClient(host=conf.get(_section2, 'ip'), port=conf.getint(_sectio
                         user=conf.get(_section2, 'user'), password=conf.get(_section2, 'pw'),
                         db=conf.get(_section2, 'db'))
 
-# pool = redis.ConnectionPool(host=conf.get(_section3, 'ip'), port=conf.get(_section3, 'port'),
-#                             password=conf.get(_section3, 'pw'),
-#                             decode_responses=True)
 pool = redis.ConnectionPool(host=conf.get(_section3, 'ip'), port=conf.get(_section3, 'port'),
+                            password=conf.get(_section3, 'pw'),
                             decode_responses=True)
+# pool = redis.ConnectionPool(host=conf.get(_section3, 'ip'), port=conf.get(_section3, 'port'),
+#                             decode_responses=True)
 
 redisCli = redis.Redis(connection_pool=pool)
